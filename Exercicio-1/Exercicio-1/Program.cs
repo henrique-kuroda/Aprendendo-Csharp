df9e2a004f0e1b9eeebd34c337e4f64fe63fa4f6
Henrique Kuroda <119912314+henrique-kuroda@users.noreply.github.com>
2025-03-14 13:04:59 -0300

    Primeiro exercício

---
+++ Exercicio-1/Exercicio-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Insira os dois números que deseja obter a soma: ");
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+
+        int soma = n1 + n2;
+
+        Console.WriteLine($"O resultado da soma dos dois valores foi: {soma}");
+    }
+}
