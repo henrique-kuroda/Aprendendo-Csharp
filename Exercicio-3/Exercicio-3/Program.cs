7569664979287428c4d4d789120781ac246ddf3c
Henrique Kuroda <119912314+henrique-kuroda@users.noreply.github.com>
2025-03-15 02:50:51 -0300

    Reupando arquivos

---
+++ Exercicio-3/Exercicio-3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Faça um Programa que peça dois números e imprima a soma.
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Insira os dois números que deseja obter a soma: ");
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+
+        int soma = n1 + n2;
+
+        Console.WriteLine($"O resultado da soma dos dois valores foi: {soma}");
+    }
+}
---
+++ Exercicio-4/Exercicio-4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Insira suas 4 notas bimestrais para obter a média: ");
+
+        float n1 = float.Parse(Console.ReadLine());
+        float n2 = float.Parse(Console.ReadLine());
+        float n3 = float.Parse(Console.ReadLine());
+        float n4 = float.Parse(Console.ReadLine());
+
+        float media = (n1 + n2 + n3 + n4) / 4;
+
+        Console.WriteLine($"A média das 4 notas foi: {media}");
+
+    }
+}
