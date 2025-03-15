6005925a705e4c0ca439c83ff9f22f21265b91df
Henrique Kuroda <119912314+henrique-kuroda@users.noreply.github.com>
2025-03-15 02:26:28 -0300

    Adicionando mais 2 exercícios

---
+++ Exercicio-8/Exercicio-8/Program.cs
@@ -0,0 +1,19 @@
+﻿//Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+//Calcule e mostre o total do seu salário no referido mês.
+using System;
+
+class Program{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe quanto você ganha por hora: ");
+        float ganho = float.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o número de horas que você trabalhou no mês: ");
+        int horas = int.Parse(Console.ReadLine());
+
+        float total = horas * ganho;
+
+        Console.WriteLine($"Seu total ganho neste mês foi: R$ {total:F2}");
+
+    }
+}
+
