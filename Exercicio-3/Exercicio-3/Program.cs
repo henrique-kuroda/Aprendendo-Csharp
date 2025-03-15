e16be28e91c08bbf523a95345ca5673a202123f2
Henrique Kuroda <119912314+henrique-kuroda@users.noreply.github.com>
2025-03-15 01:14:02 -0300

    Exercicio 3

---
+++ Exercicio-3/Exercicio-3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Faça um Programa que converta metros para centímetros.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um valor em metro para ser convertido em centimetros: ");
+        float metros = float.Parse(Console.ReadLine());
+
+        float centimetros = metros * 100;
+        Console.WriteLine($"O valor transformado para centímetros ficou: {centimetros} cm");
+    }
+}
+
+
+
