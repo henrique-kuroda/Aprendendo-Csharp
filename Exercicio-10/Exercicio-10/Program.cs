2a8315acf8c5b640baa47b64e0f872fd89ea8555
Henrique Kuroda <119912314+henrique-kuroda@users.noreply.github.com>
2025-03-16 15:15:44 -0300

    Exercício 10

---
+++ Exercicio-10/Exercicio-10/Program.cs
@@ -0,0 +1,15 @@
+﻿//Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe uma temperatura em graus Celsius para ser transformada em graus Fahrenheit: ");
+        float celsius = float.Parse(Console.ReadLine());
+
+        float fahrenheit = (9 * (celsius) / 5) + 32;
+
+        Console.WriteLine($"A temperatura em graus Farenheit foi: {fahrenheit:F2} °F");
+    }
+}
