7ca58101b3c4cba87602986ebfcad266734811cf
Henrique Kuroda <119912314+henrique-kuroda@users.noreply.github.com>
2025-03-18 13:59:26 -0300

    Reupando arquivos

---
+++ Exercicio-006/Exercicio-006/Program.cs
@@ -0,0 +1,15 @@
+﻿//Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o raio do circulo para obter sua área: ");
+
+        double raio = double.Parse(Console.ReadLine());
+        double area = Math.Pow(raio, 2) * Math.PI;
+
+        Console.WriteLine($"A área do circulo de raio {raio} é: {area:F2}");
+    }
+}
