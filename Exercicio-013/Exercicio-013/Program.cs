3f60050ab27459dfdf01dd0eb3b3e7e863dca444
Henrique Kuroda <119912314+henrique-kuroda@users.noreply.github.com>
2025-03-18 13:50:03 -0300

    Exercício 13

---
+++ Exercicio-013/Exercicio-013/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Exercicio 013
+Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+Para homens: (72.7h) - 58 Para mulheres: (62.1h) - 44.7
+*/
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Para descobrir o seu peso ideal informe a sua altura em m: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Para homens digite (h) e para mulheres digite (m)");
+        char option = char.Parse(Console.ReadLine());
+
+        double resultado;
+        if(option == 'h' || option == 'H'){
+            resultado = (72.7 * altura) - 58;
+            Console.WriteLine($"O seu peso ideal é: {resultado:F2}kg");
+        }
+        else if(option == 'm' || option == 'M'){
+            resultado = (62.1 * altura) - 44.7;
+            Console.WriteLine($"O seu peso ideal é: {resultado:F2}kg");
+        }
+        else{
+            Console.WriteLine("Opção inválida!");
+        }
+
+    }
+}
