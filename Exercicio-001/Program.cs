71544699a2a954cca1260f28205ca58b8976703f
Henrique Kuroda <119912314+henrique-kuroda@users.noreply.github.com>
2025-03-18 21:47:19 -0300

    Exercício 1

---
+++ Exercicio-001/Program.cs
@@ -0,0 +1,11 @@
+﻿//Exercicio 001
+//Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Alo mundo");
+    }
+}
