5d6e668b4d011d6f6d5b5b75e32a9b06004c04af
Henrique Kuroda <119912314+henrique-kuroda@users.noreply.github.com>
2025-03-18 21:42:31 -0300

    Exercicio2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Exercicio 002
+//Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe um número: ");
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine($"O número informado foi {num}");
+    }
+}
